01a66363063e4cbade1a7a1d97634f88c13b1d1f
francescobarbarulo <barbarulofrancesco@gmail.com>
2025-07-23 16:49:05 +0200

    fix get lab

--- bashly/src/get_command.sh
+++ bashly/src/get_command.sh
@@ -3,5 +3,5 @@ WORKSPACE="${lab_name}-${ENV}"
 
 cd $PWD/terraform
 terraform workspace select ${WORKSPACE} &> /dev/null || (echo "Lab \"${lab_name}\" does not exist" && exit 1)
-echo "$(terraform output -raw hostname) ($(terraform output -raw state))"
+echo "$(terraform output -raw lab_url) ($(terraform output -raw state))"
 cd $PWD/..
